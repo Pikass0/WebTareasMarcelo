bd10bb54a9b7c3acd73149045d66acb60b19e8a4
Marcelo <user@PC524>
2020-12-22 11:58:57 +0100

    refactor nombre proyecto

--- src/main/java/com/tarea/services/DB.java
+++ src/main/java/com/tarea/services/DB.java
@@ -10,14 +10,14 @@ import java.util.Set;
 
 public class DB {
     private static Map<String, Set<Integer>> tareasUsuario;  //username, id tareas
-    private static Map<Integer, Tarea> tareas;
+    private static Map<Integer, Tarea> tareas; //id tarea como key, tarea
     private static Set<Usuario> usuarios;
     private static int ultimoIdTarea = 4;
 
     static{
         //inicializacion de Tareas
         tareas = new HashMap<>();
-        tareas.put(1, new Tarea(ultimoIdTarea, descripcion));
+        tareas.put(1, new Tarea(1, "Comprar pan"));
         
         
     }
