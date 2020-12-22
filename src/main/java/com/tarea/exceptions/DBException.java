3b0da9b256b2402325f2bcc9837abd624c3ec50c
Marcelo <user@PC524>
2020-12-22 11:55:45 +0100

    refactor nombre proyecto

---
+++ src/main/java/com/tarea/exceptions/DBException.java
@@ -0,0 +1,11 @@
+
+package com.tarea.exceptions;
+
+
+public class DBException extends Exception{
+
+    public DBException(String msg) {
+        super(msg);
+    }
+    
+}
--- src/main/java/com/tarea/services/DB.java
+++ src/main/java/com/tarea/services/DB.java
@@ -3,6 +3,7 @@ package com.tarea.services;
 
 import com.tarea.model.Tarea;
 import com.tarea.model.Usuario;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,7 +12,23 @@ public class DB {
     private static Map<String, Set<Integer>> tareasUsuario;  //username, id tareas
     private static Map<Integer, Tarea> tareas;
     private static Set<Usuario> usuarios;
-    private static int ultimoIdTarea = 0;
+    private static int ultimoIdTarea = 4;
 
+    static{
+        //inicializacion de Tareas
+        tareas = new HashMap<>();
+        tareas.put(1, new Tarea(ultimoIdTarea, descripcion));
+        
+        
+    }
+    
+    
+    //para que nadie haga new
+    private DB() {
+    }
+
+     
+    
+    
     
 }
